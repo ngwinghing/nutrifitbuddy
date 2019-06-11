e9914e7dc54decf63ca024d26ad2faa7469c1e5d
ngwinghing <wing.nwh@gmail.com>
2019-06-11 12:52:51 +0800

    Welcome

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { AppLoading, Asset } from 'expo';
+
+import Navigation from '../navigation';
+import { Block } from './components';
+
+const images = [
+  require('./assets/images/back.png'),
+  require('./assets/images/plants.png'),
+  require('./assets/images/seeds.png'),
+  require('./assets/images/flowers.png'),
+  require('./assets/images/sprayers.png'),
+  require('./assets/images/pots.png'),
+  require('./assets/images/fertilizers.png'),
+  require('./assets/images/plants_1.png'),
+  require('./assets/images/plants_2.png'),
+  require('./assets/images/plants_3.png'),
+  require('./assets/images/explore_1.png'),
+  require('./assets/images/explore_2.png'),
+  require('./assets/images/explore_3.png'),
+  require('./assets/images/explore_4.png'),
+  require('./assets/images/explore_5.png'),
+  require('./assets/images/explore_6.png'),
+  require('./assets/images/avatar.png'),
+];
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  }
+
+  handleResourcesAsync = async () => {
+    // we're caching all the images
+    // for better performance on the app
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    return Promise.all(cacheImages);
+  }
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.handleResourcesAsync}
+          onError={error => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      )
+    }
+
+    return (
+      <View style={styles.container}>
+        <Navigation />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,1 +1,18 @@
-Welcome.js
+import React, { Component } from 'react'
+import { StyleSheet } from 'react-native'
+
+import { Button, Block, Text } from '../components';
+
+export default class Welcome extends Component {
+	render() {
+		return (
+			<View>
+				<Text> Welcome </Text>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+
+})
